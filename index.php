49f97a0cb2424012bb9d3180a2e8d1ed2651c7b8
jiji15899-arch <jiji15899@gmail.com>
2026-01-22 15:42:20 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Index Template
+ * 글쓰기 스킨은 홈화면이 없으므로 archive로 리다이렉트
+ */
+
+get_header();
+?>
+
+<div class="container">
+    <div style="text-align: center; padding: 100px 20px;">
+        <h2>이 사이트는 글 목록 페이지가 없습니다.</h2>
+        <p>개별 글을 직접 방문해주세요.</p>
+    </div>
+</div>
+
+<?php
+get_footer();
+?>
